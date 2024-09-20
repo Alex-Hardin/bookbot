74bce44fdf9d35f9a4e78826c06c17488b56a448
Alex-Hardin <johaleia@protonmail.com>
2024-09-20 17:26:36 -0500

    working word count function

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        return file_contents
-main()
+    with open("/home/johaleia/workspace/github.com/Alex-Hardin/bookbot/books/frankenstein.txt") as f:
+        read_contents = f.read()
+        print(read_contents)
+
+def word_count():
+    # Grab frankenstein.txt with path and call it frankenstein
+    with open("/home/johaleia/workspace/github.com/Alex-Hardin/bookbot/books/frankenstein.txt") as frankenstein:
+        # Word counter
+        words = 0
+        # Loop through each word in frankenstein
+        for word in frankenstein:
+            # Word.split() splits each line into a list of words based on whitespace
+            words_split = word.split()
+            # Add the number of words to the counter above.
+            words += len(words_split)
+    print(words)
+
+word_count()
