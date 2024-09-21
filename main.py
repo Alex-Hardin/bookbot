378799f10dad95a95c65206f0d3070c64814ecb0
Alex-Hardin <johaleia@protonmail.com>
2024-09-21 16:55:49 -0500

    working bootbot

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+"""
 def main():
         with open("/home/johaleia/workspace/github.com/Alex-Hardin/bookbot/books/frankenstein.txt") as f: 
             read_contents = f.read()
         print(read_contents)
+"""
 
 def word_count():
     # Grab frankenstein.txt with path and call it frankenstein
@@ -30,15 +32,21 @@ def character_count():
                 else:
                     characters_counted[char] = 1
     letters_only = {char: count for char, count in characters_counted.items() if ('a' <= char <= 'z')}
-    return(letters_only)
+    letters_list = [{"char": char, "num": count} for char, count in letters_only.items()]
+    letters_list.sort(reverse=True, key=dict_sort)
+    return letters_list
+   
+   
 
 def dict_sort(dict):
     return dict["num"]
 
 
 def print_report():
+    new_list = character_count()
     print(f"--- Begin report of books/frankenstein.txt --- \n{word_count()} words found in the document")
+    for item in new_list:
+        print(f"The '{item['char']}' character was found {item['num']} times")
+    print("--- End report ---")
 
-character_count()
-
-
+print_report()
